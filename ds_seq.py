2c65877f0ed258d84cc0f2188e3175366ed954ad
SparshRajGupta <sparsh.raj.1993@gmail.com>
2017-04-12 12:39:57 -0700

    Data Structure using sequence

---
+++ ds_seq.py
@@ -0,0 +1,30 @@
+shoplist =['apple','mango','peach','carrots']
+name = 'swaroop'
+
+print 'Item 0 is: ',shoplist[0]             #indexing operation
+print 'Item 1 is: ',shoplist[1]
+print 'Item 2 is: ',shoplist[2]
+print 'Item 3 is: ',shoplist[3]
+
+print '\n'
+
+print 'Item -1 is: ',shoplist[-1]
+print 'Item -2 is: ',shoplist[-2]
+
+print '\n'
+
+print 'Item 1 to 3 are: ',shoplist[1:3]     #slicing on a list
+print 'Item 2 to end : ',shoplist[2:]
+print 'Item 1 to -1 are: ',shoplist[1:-1]
+print 'Item start to end are: ',shoplist[:]
+
+print '\n'
+
+print 'Charecters 1 to 3 are: ',name[1:3]   #slicing on a string
+print 'Charecters 2 to end: ',name[2:]
+print 'Charecters 1 to -1 are: ',name[1:-1]
+print 'Charecters start to end : ',name[:]
+
+
+
+
