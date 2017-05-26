078af29f9e6d78424686cf8a9ada4539003f64c2
SparshRajGupta <sparsh.raj.1993@gmail.com>
2017-05-26 11:15:23 -0700

    Practicing if-else

---
+++ second_largest_num.py
@@ -0,0 +1,8 @@
+n = int(raw_input("Enter a num: "))
+input_list = []
+if n >= 2 and n <= 10:
+    input_list = [int(x) for x in raw_input("Enter an array of nos. seperated by space: ").split() if (int(x) >= -100 and int(x) <= 100)]
+    new_list = list(set(input_list))
+    new_list.sort()
+    print ("New Sorted List:{}").format(new_list)
+    print ("Second Largest No.: {}").format(new_list[-2])
