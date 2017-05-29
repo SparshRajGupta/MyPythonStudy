87c64be30f0ff271955af00ffc084bf99f2d7295
SparshRajGupta <sparsh.raj.1993@gmail.com>
2017-05-29 15:45:01 -0700

    using type() and isinstance() function

---
+++ checking_data_type.py
@@ -0,0 +1,11 @@
+a = 5
+print (a, "is of type",type(a))
+
+b = 3.0
+print (b, "is of type",type(b))
+
+c = "Hello"
+print (c, "is of type", type(c))
+
+d = 1+2j
+print (d,"is complex number?",isinstance(d,complex))
---
+++ nested_list.py
@@ -0,0 +1,5 @@
+n = int(raw_input("Enter no. of students: "))
+name =[]
+
+for i in range (0,n):
+    name =
